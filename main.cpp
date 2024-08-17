8330ad3275ce0dbf8e29782ab86a2ff38bcc4757
choutaLAN <117169061+choutaLAN@users.noreply.github.com>
2024-08-17 10:49:35 +1000

    learning about decelerations and assignments

--- main.cpp
+++ main.cpp
@@ -2,10 +2,17 @@
 
 using namespace std;
 int main(){
-    std::cout <<"Hello World!";
-    string x;
-    x = " Mice.";
-    std::cout << x;
+    std::cout << "Hello World!" << "\n";
+    string z; //deceleration.
+    z = " Mice."; //assignment.
+    int x = 5; //deceleration and assignment.
+    int y = 6; //deceleration and assignment.
+
+    std::cout << z << "\n"; // "\n" means a new line in the output
+    std::cout << x << "\n";
+    std::cout << y << "\n";
+    std::cout << y+x;
+    
 
     return 0;
 }
